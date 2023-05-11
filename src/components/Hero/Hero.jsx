6bc76684b42ce883022e045fff4717c46fc8e762
ApostolosFotopoulos <apfotopoulos1997@gmail.com>
2023-05-11 23:48:53 +0300

    created about section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
 import TitleBg from "../../images/pricing/titlebg.svg";
+import MainButton from "../MainButton";
 
 function Hero() {
   return (
     <>
       <section className="hero-section w-full h-screen">
-        <div className="container">
+        <div className="container-hero">
+          {/* hero section */}
           <div
             className="flex flex-col text-white
            absolute hero-text "
@@ -15,18 +16,18 @@ function Hero() {
             </p>
             <img
               src={TitleBg}
+              alt="text_bg"
               className=" w-[52%] absolute -top-3 title-white "
             />
             <h1 className="font-bold text-[48px] uppercase mb-[4rem]">
               make your body <br />
               <span className="font-thin">fit & perfect</span>
             </h1>
-            <Link className="text-[15px] bg-white text-black text-center pt-[18px] font-[600] w-[20rem] h-[5.5rem] uppercase hero-cta relative ml-2 ">
-              Our Classes <i class="fa-solid fa-arrow-right text-[#FF0336]"></i>
-            </Link>
+
+            <MainButton color={`text-black`} bg={`bg-white`} />
           </div>
         </div>
-
+        {/* socials */}
         <div
           className="flex flex-row-reverse -rotate-90 text-white
           absolute top-2/4 right-0 items-center"
@@ -34,9 +35,9 @@ function Hero() {
           <p className="uppercase  text-3xl font-bold tracking-widest">share</p>
           <span className="w-[35px] bg-[#FF0336] h-[2.5px] mr-6"></span>
           <div className="text-white gap-7 flex mr-7 ">
-            <i className="fa-brands rotate-90 hover:text-[#FF0336] fa-facebook text-3xl cursor-pointer"></i>
-            <i className="fa-brands rotate-90 hover:text-[#FF0336] fa-twitter text-3xl cursor-pointer"></i>
-            <i className="fa-brands rotate-90 hover:text-[#FF0336]  fa-linkedin-in text-3xl cursor-pointer "></i>
+            <i className="fa-brands rotate-90 hover:text-[#FF0336] fa-facebook text-3xl cursor-pointer ease-in duration-200 "></i>
+            <i className="fa-brands rotate-90 hover:text-[#FF0336] fa-twitter text-3xl cursor-pointer ease-in duration-200"></i>
+            <i className="fa-brands rotate-90 hover:text-[#FF0336]  fa-linkedin-in text-3xl cursor-pointer ease-in duration-200 "></i>
           </div>
         </div>
       </section>
